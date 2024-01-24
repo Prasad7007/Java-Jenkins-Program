bedab1a026ef4344ad2a6433d0d80ad5736aab3d
Prasad7007 <prasadagalave0007@gmail.com>
2024-01-24 22:24:31 +0530

    Initial Commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
         System.out.println("This is java program");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("Created Chenges!");
     }
 }
